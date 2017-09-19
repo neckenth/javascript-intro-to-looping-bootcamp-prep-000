7c41195abbcf5b37e176a8837f6c814c58f3df5b
Nancy Eckenthal <nancy.eckenthal@gmail.com>
2017-09-19 02:24:27 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,7 +4,7 @@ function forLoop(array) {
             array = ["I am 1 strange loop", ...array];
         }
         else if i != 1 {
-            array = ["I am "  + i + "strange loop", ...array]);
+            array = ["I am "  + i + " strange loop", ...array]);
         }
     }
     return array
