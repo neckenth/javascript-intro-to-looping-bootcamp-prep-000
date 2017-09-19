67b236ef43603a4e281e3edefe6ed02734e7f384
Nancy Eckenthal <nancy.eckenthal@gmail.com>
2017-09-19 02:21:24 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop(array) {
     for (let i = 0;, i < 25; i++) {
         if i == 1 {
-            array.push("I am 1 strange loop.");
+            array = ["I am 1 strange loop", ...array];
         }
-        else if i > 1 {
-            array.push("I am " + i + "strange loops.");
+        else if i != 1 {
+            array = ["I am "  + i + "strange loop", ...array]);
         }
     }
     return array
